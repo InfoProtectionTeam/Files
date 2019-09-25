a087aac45e2cf65f935591e4b47e088d103c2ad0
Kevin McKinnerney <40665896+kemckinnmsft@users.noreply.github.com>
2019-09-25 00:21:58 -0500

    Update New-AIPAuthTokenUL.ps1

--- Scripts/AIPScanner/New-AIPAuthTokenUL.ps1
+++ Scripts/AIPScanner/New-AIPAuthTokenUL.ps1
@@ -35,7 +35,6 @@ $ReqAccessUL.ResourceAppId = $SvcPrincipalUL.AppId
 $Role4 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "8b2071cd-015a-4025-8052-1c0dba2d3f64", "Role"
 $ReqAccessUL.ResourceAccess = $Role4
 
-"Creating Native Application $CKI"
 New-AzureADApplication -DisplayName $DisplayName -ReplyURLs http://localhost -RequiredResourceAccess @($ReqAccess, $ReqAccessUL)
 $WebApp = Get-AzureADApplication -Filter "DisplayName eq '$DisplayName'"
 New-AzureADServicePrincipal -AppId $WebApp.AppId
@@ -43,8 +42,10 @@ $WebAppKey = New-Guid
 $Date = Get-Date
 New-AzureADApplicationPasswordCredential -ObjectId $WebApp.ObjectID -startDate $Date -endDate $Date.AddYears(1) -Value $WebAppKey.Guid -CustomKeyIdentifier "Password"
 $TenantID = (Get-AzureADCurrentSessionInfo).tenantid
+
 "Generating Authenitcation Token scripts for AIP Scanner Service"    
 Start-Sleep -Seconds 5
+
 '"A browser will launch to the created web application to provide Admin consent for the required API permissions. Please log in with tenant admin credentials to provide permissions for this application.  If you are unable to provide this consent, please provide the URL below to your tenant administrator."' | Out-File ~\Desktop\Grant-AdminConsentUL.ps1
 '$weburl = "https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/CallAnAPI/appId/'+$WebApp.AppId+'/isMSAApp/"' | Out-File ~\Desktop\Grant-AdminConsentUL.ps1 -Append
 "" | Out-File ~\Desktop\Grant-AdminConsentUL.ps1 -Append
@@ -54,10 +55,11 @@ Start-Sleep -Seconds 5
 'Pause' | Out-File ~\Desktop\Grant-AdminConsentUL.ps1 -Append
 'Start-Process $weburl' | Out-File ~\Desktop\Grant-AdminConsentUL.ps1 -Append
 'Pause' | Out-File ~\Desktop\Grant-AdminConsentUL.ps1 -Append
+
 '$ServiceAccount = Get-Credential "Enter the on-premises service account credentials"' | Out-File ~\Desktop\Set-AIPAuthenticationUL.ps1 -Append
 "Set-AIPAuthentication -AppID " + $WebApp.AppId + " -AppSecret " + $WebAppKey.Guid + " -TenantID " + $TenantID.Guid + ' -OnBehalfOf $ServiceAccount' | Out-File ~\Desktop\Set-AIPAuthenticationUL.ps1 -append
 ""
-"Authenitcation Token script stored on the desktop as Set-AIPAUthenticationUL.ps1"
+"Authenitcation Token scripts stored on the desktop as Grant-AdminConsentUL.ps1 and Set-AIPAUthenticationUL.ps1"
 ""
 "Follow the instructions at https://aka.ms/ScannerBlog to install the service"
 ""
