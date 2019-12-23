45deaa9a148b26fa8ef086d8167bf5f8b724a61d
Kevin McKinnerney <40665896+kemckinnmsft@users.noreply.github.com>
2019-12-23 16:43:08 -0600

    Add files via upload

---
+++ Scripts/tenantinfo.ps1
@@ -0,0 +1,11 @@
+﻿"Enter Domain Name (e.g. AIPDemo.com)"
+$domain = Read-Host
+$config = (Invoke-WebRequest –Uri "https://login.microsoftonline.com/$domain/.well-known/openid-configuration").content | ConvertFrom-Json
+
+"$domain configuration"
+""
+"Tenant Id"
+($config.token_endpoint -split('/'))[3]
+""
+"Region"
+$config.tenant_region_scope
